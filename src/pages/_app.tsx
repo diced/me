27f5d3eebc8dbd858923bd592094e1ae821b3096
diced <pranaco2@gmail.com>
2023-08-21 20:31:48 -0700

    faivon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import "@/styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
 import { Inter } from "next/font/google";
+import Head from "next/head";
 import { useRouter } from "next/router";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -12,13 +13,24 @@ export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   return (
-    <ThemeProvider attribute="class">
-      <main className={inter.className}>
-        <Component {...pageProps} />
-      </main>
+    <>
+      <Head>
+        <title>dicedtomato</title>
 
-      {router.pathname !== "/" && <BackButton />}
-      <ThemeSwitcher />
-    </ThemeProvider>
+        <meta name="title" content="dicedtomato" />
+        <meta name="description" content="hi" />
+        <meta property="theme-color" content="#000000" />
+
+        <link rel="icon" href="/favicon.png" type="image/png" />
+      </Head>
+      <ThemeProvider attribute="class">
+        <main className={inter.className}>
+          <Component {...pageProps} />
+        </main>
+
+        {router.pathname !== "/" && <BackButton />}
+        <ThemeSwitcher />
+      </ThemeProvider>
+    </>
   );
 }
